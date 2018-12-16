9baaad055d946de16254df5150336e0c6961df57
Nasato Goto <7310510@gmail.com>
2018-12-16 18:28:23 +0900

    WIP: install cobra

--- cmd/tapirjr/main.go
+++ cmd/tapirjr/main.go
@@ -1,16 +1,6 @@
 package main
 
-import (
-	"github.com/7310510/tapirjr/internal/pkg/client"
-	"github.com/7310510/tapirjr/internal/pkg/server"
-)
-
 func main() {
-	execute()
 
-	go server.Run(globalOpts.GobgpAddr, globalOpts.Port)
-	go client.Run(globalOpts.GobgpAddr, globalOpts.PeerAddrs)
-
-	for {
-	}
+	newRootCmd().Execute()
 }
